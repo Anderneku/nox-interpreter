09718fc72be4ebcd569e8d3aa1bc8e8f73b1bb9d
SiXFeet <68037779+Anderneku@users.noreply.github.com>
2025-12-30 18:38:58 +0100

    Added parser module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod types;
 mod lexer;
-use crate::{lexer::lex, types::*};
+use crate::{lexer::lex};
 
 
 fn main() {
-    let source :&str = "let print(34);";
+    let source :&str = "print(34);";
     println!("{:?}", lex(source));
 }
